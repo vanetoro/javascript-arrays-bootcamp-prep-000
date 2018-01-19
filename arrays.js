82ac2f31d4244b3cc8378406f14f153d41a0d450
Vanessa Toro <vanetoro84@gmail.com>
2018-01-19 22:26:29 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,48 @@
+var ingredient1 = 'bread';
+var ingredient2 = 'mild cheese';
+var ingredient3 = 'sharp cheese';
+var ingredient4 = 'butter';
+var ingredient5 = 'tomato';
+var ingredient6 = 'garlic';
+
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ]
+  
+  ['butterfinger', ...chocolateBars];
+  
+  function addElementToBeginningOfArray(arr, x){
+    var newArr = arr.unshift(x);
+    return newArr;
+    
+  }
+
+  function destructivelyAddElementToBeginningOfArray(arr,x){
+    arr.unshift(x);
+    return arr;
+  }
+  
+  function addElementToEndOfArray(arr,x){
+    var newArr = arr.push(x)
+    return newArr;
+  }
+  
+  function destructivelyAddElementToEndOfArray(arr,x){
+    arr.push(x);
+    return arr;
+  }
+  
+  function accessElementInArray(arr,element){
+    return arr[element];
+    
+  }
+  
+  function destructivelyRemoveElementFromBeginningOfArray(arr){
+    return arr.shift()
+  }
+    
+  
+  
